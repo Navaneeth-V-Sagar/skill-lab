b3f6572cb6a721c305b4245bcf803dfff38930a2
Navaneeth V Sagar <navaneethsagar.v@gmail.com>
2024-11-30 10:49:42 +0530

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <h1>Flavorscape</h1>
+      </div>
+      <nav className="navigation">
+        <ul>
+          <li><Link to="/">Homeco</Link></li>
+          <li><Link to="/categories">Categories</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
